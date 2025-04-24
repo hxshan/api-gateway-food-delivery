8ebe836ae31613d6ac766de936c6026284d713ab
hxshan-g <heshan.g@syncturma.com>
2025-04-24 19:26:38 +0530

    cors

--- src/config/corsOptions.js
+++ src/config/corsOptions.js
@@ -1,12 +1,19 @@
-import cors from 'cors';
+
+const allowedOrigins = [
+  "http://localhost:3000",
+  "http://localhost:5000",
+  "0.0.0.0"   
+];
 
 export const corsOptions = {
-  origin: [
-    'http://localhost:3000', 
-    'https://frontend.com', 
-  ],
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true, 
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
 };
-
